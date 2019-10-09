7f00dd422f42bf3f5d600d53bd1db2bf7b9c7406
Paul Komurka <pawlex@gmail.com>
2019-10-09 00:57:27 -0700

    addl. update checks

--- tunnelbroker_update.sh
+++ tunnelbroker_update.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-USERNAME=""
-APIKEY=""
-TUNNEL_ID=""
-
 # Load configuration from file if it exists.
 # You may also define your credendials inline (below)
 BASE="$(dirname "$0")"
@@ -12,6 +8,37 @@ if [ -e $BASE/tunnelbroker.cf ]; then
     source $BASE/tunnelbroker.cf
 fi
 
+#USERNAME=""
+#APIKEY=""
+#TUNNEL_ID=""
+#P2P_LOCAL=""
+#P2P_REMOTE=""
+
+
+# Need to make sure the system has ping6 installed.
+if hash ping6 2>/dev/null; then
+    PING6=`which ping6` 
+else
+    printf "ping6 not installed"
+    exit -1
+fi
+
+# Abort if local side of the tunnel isn't configured.
+RETVAL=$(eval $PING6 -c 1 -X 1 ${P2P_LOCAL})
+
+if [ $? != 0 ]; then 
+	echo "Local side of the tunnel is not configured.  exiting"
+	exit -1
+fi
+
+# Next, check to see if we can ping the remote side of the Tunnel.
+# If so, then there is no need to update and we should exit.
+
+RETVAL=$(eval $PING6 -c 1 -X 1 ${P2P_REMOTE})
+if [ $? == 0 ]; then
+	echo "IPv6 functional.  No update necessary.  exiting"
+	exit 0
+fi
 
 # Need to make sure the system has CURL installed.
 if hash curl 2>/dev/null; then
